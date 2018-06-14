0c879bdf25807dd74bacb96d29d05ab22a67b4e0
samsonjj <samsonjj@vcu.edu>
2018-06-14 17:43:29 -0400

    second commit

--- api/models/brotherListModel.js
+++ api/models/brotherListModel.js
@@ -3,3 +3,14 @@ function Brother(rollNumber, firstName, lastName) {
 	this.firstName = firstName;
 	this.lastName = lasName;
 }
+
+exports = class Brother {
+
+	constructor(rollNumber, firstName, lastName, ) {
+		this.rollNumber = rollNumber;
+		this.firstName = firstName;
+		this.middleName = middleName;
+		this.lastName = lastName;
+	}
+
+}
