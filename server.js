f539381c4a83db62e271dc626adda27af986c6fa
Jonathan <samsonjj@vcu.edu>
2018-07-01 16:23:19 -0400

    changes from old project

--- server.js
+++ server.js
@@ -14,13 +14,18 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
+// allow access using access-control-allow-origin header
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 
 var routes = require('./api/routes/brotherRoutes');
-
-
-
 routes(app);
 
-app.listen(config.port);
 
+
+app.listen(config.port);
 console.log('kgotweb server started on: ' + config.port);
